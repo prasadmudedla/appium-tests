7e32cb30f2f3868ac47a702e836bd349c433b660
Prasad Mudedla <prasadmudedla@live.in>
2020-09-23 01:11:17 +0530

    Added the NZ test

--- src/test/java/BrowserTest.java
+++ src/test/java/BrowserTest.java
@@ -1,11 +1,13 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -14,6 +16,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class BrowserTest {
 
@@ -24,54 +27,53 @@ public class BrowserTest {
     public void setUp() throws MalformedURLException {
         caps = new DesiredCapabilities();
         caps.setCapability(MobileCapabilityType.DEVICE_NAME, "android");
-        caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, "7.1.1");
+        caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10.0");
         caps.setCapability(MobileCapabilityType.BROWSER_NAME,"Chrome");
-        caps.setCapability(MobileCapabilityType.BROWSER_VERSION, "85");
+        caps.setCapability(MobileCapabilityType.BROWSER_VERSION, "74");
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), caps);
+        driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
     }
 
     @Test
-    public void BrowserTest() {
+    public void BrowserTest() throws InterruptedException {
         driver.get("https://www.nzta.govt.nz/");
-        driver.get("https://www.nzta.govt.nz/roads-and-rail/toll-roads/");
-        scrollDowntoXPath("//span[text()='Pay a toll']", driver);
-        WebElement payATollLink = driver.findElement(By.xpath("//span[text()='Pay a toll']"));
+        WebElement payATollLink = driver.findElement(By.xpath("//div[4]//ul/li[5]/a/span"));
         payATollLink.click();
+        WebElement buyOrPayAToll = driver.findElement(By.xpath("/html/body/div[4]/div/div/div[1]/div/div/div[1]/p[1]/a"));
+        buyOrPayAToll.click();
+        WebElement declareReg = driver.findElement(By.xpath("/html/body/div[5]/ui-view/div[2]/div/div/form/fieldset[1]/div/div[2]/ul/li/input"));
+        declareReg.click();
+        WebElement continueButton = driver.findElement(By.xpath("/html/body/div[5]/ui-view/div[2]/div/div/form/fieldset[2]/div/span/input"));
+        continueButton.click();
+        WebElement plateField = driver.findElement(By.xpath("/html/body/div[5]/ui-view/div[2]/div/div/form/fieldset[1]/div/span/input"));
+        plateField.sendKeys("CYJ421");
+        WebElement numberContinueButton = driver.findElement(By.xpath("/html/body/div[5]/ui-view/div[2]/div/div/form/fieldset[2]/div/span/input"));
+        numberContinueButton.click();
+        Thread.sleep(5000);
+        WebElement confirmContinue = driver.findElement(By.xpath("/html/body/div[5]/ui-view/div[2]/div/div/form/div/span/input"));
+        confirmContinue.click();
+        WebElement northernGateToll = driver.findElement(By.xpath("/html/body/div[5]/ui-view/div[2]/div/div/form/div[1]/div[1]/div/span/input"));
+        WebElement taurangaToll = driver.findElement(By.xpath("/html/body/div[5]/ui-view/div[2]/div/div/form/div[2]/div[1]/div/span/input"));
+        WebElement takitimuToll = driver.findElement(By.xpath("/html/body/div[5]/ui-view/div[2]/div/div/form/div[3]/div[1]/div/span/input"));
+        northernGateToll.sendKeys("1");
+        taurangaToll.sendKeys("2");
+        takitimuToll.sendKeys("3");
+        WebElement tollPriceContinue = driver.findElement(By.xpath("/html/body/div[5]/ui-view/div[2]/div/div/form/div[4]/span/input"));
+        tollPriceContinue.click();
+        WebElement noEmail = driver.findElement(By.xpath("/html/body/div[5]/ui-view/div[2]/div/div/div/form/div[1]/div/ul/li[2]/label"));
+        noEmail.click();
+        WebElement noEmailConfirmButton = driver.findElement(By.xpath("/html/body/div[5]/ui-view/div[2]/div/div/div/form/div[2]/span/input"));
+        noEmailConfirmButton.click();
+        WebElement payNowButton = driver.findElement(By.xpath("/html/body/div[5]/div[2]/div[2]/span/input"));
+        payNowButton.click();
+
     }
 
-    public static void scrollDowntoXPath(String xPath, AppiumDriver driver) {
-        boolean flag=true;
-        int count=1;
-        while(flag){
-            try {
-                driver.findElement(By.xpath(xPath));
-                flag=false;
-                break;
-            }
-            catch(Exception NoSuchElementException) {
-                count=count+1;
-                Map<String, Object> params = new HashMap<>();
-                params.put("start","40%,90%");
-                params.put("end","40%,20%");
-                params.put("duration","2");
-                Object res= driver.executeScript("mobile:touch:swipe",params);
-                if(count==5)
-                {
-                    break;
-                }
-            }
-        }
+    public void waitforElementXpath(String str) {
+        WebDriverWait wait = new WebDriverWait(driver,20);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(str)));
     }
 
-    public static void scroll() {
-//        HashMap<String, String> scrollObject = new HashMap<String, String>();
-//        RemoteWebElement element = (RemoteWebElement) driver.findElement(By.tagName(scrollableElement));
-//        JavascriptExecutor js = (JavascriptExecutor) driver;
-//        String widId = ((RemoteWebElement) element).getId();
-//        scrollObject.put("text", text);
-//        scrollObject.put("element", widId);
-//        js.executeScript("mobile: scrollTo", scrollObject);
-    }
 
 
 
